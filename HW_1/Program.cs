9983760d18eab70652b6c3528d482c538e98f288
Sasha <aleksandra.majerus@gmail.com>
2023-08-07 09:27:39 +0300

    HW1 is done

---
+++ HW_1/Program.cs
@@ -0,0 +1,73 @@
+﻿// Console.WriteLine("Enter the number");
+// int number= Convert.ToInt32(Console.ReadLine()); // Convert.ToInt32 переводит в числовой формат
+// int result= number*number;
+// Console.WriteLine(result);
+
+// Console.WriteLine("Enter the number:");
+// int number1= Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Enter the number:");
+// int number2= Convert.ToInt32(Console.ReadLine());
+
+// if(number1==number2*number2) //Сравнение переменных обозначается ==, присвоение =
+// {
+//     Console.WriteLine("Yes");
+// }
+// else
+// {
+//     Console.WriteLine("No");
+// }
+
+// Console.WriteLine("Enter the number:");
+// int number= Convert.ToInt32(Console.ReadLine());
+
+// if(number==1)
+// {
+//     Console.WriteLine("Monday");
+// }
+// else if(number==2)
+// {
+//     Console.WriteLine("Tuesday");
+// }
+// else if(number==3)
+// {
+//     Console.WriteLine("Wednesday");
+// }
+// else if(number==4)
+// {
+//     Console.WriteLine("Thursday");
+// }
+// else if(number==5)
+// {
+//     Console.WriteLine("Friday");
+// }
+// else if(number==6)
+// {
+//     Console.WriteLine("Saturday");
+// }
+// else if(number==7)
+// {
+//     Console.WriteLine("Sunday");
+// }
+// else
+// {
+//     Console.WriteLine("The day doesn't exist");
+// }
+
+// Console.WriteLine("Enter the number:");
+// int number= Convert.ToInt32(Console.ReadLine());
+
+// int numberNeg= number*-1;
+
+// while(numberNeg<=number)
+// {
+//     Console.WriteLine(numberNeg);
+//     numberNeg=numberNeg+1;
+// }
+
+// Console.WriteLine("Enter the number:");
+// int number= Convert.ToInt32(Console.ReadLine());
+
+// int result7= number%10;
+// Console.WriteLine(result7);
+
